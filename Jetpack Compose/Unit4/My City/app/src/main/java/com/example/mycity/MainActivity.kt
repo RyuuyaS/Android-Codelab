268a279db2db6fdabbe3c77d3559a4ad9ccff131
Poro <thinhyolo1994@gmail.com>
2023-04-29 22:56:15 +0700

    Adding elevation

--- Jetpack Compose/Unit4/My City/app/src/main/java/com/example/mycity/MainActivity.kt
+++ Jetpack Compose/Unit4/My City/app/src/main/java/com/example/mycity/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
@@ -130,10 +131,11 @@ fun TopAppBar(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .fillMaxWidth()
+            .padding(10.dp)
     ) {
         Text(
             text = title,
-            fontSize = 36.sp,
+            fontSize = 30.sp,
             fontWeight = FontWeight.Bold,
             lineHeight = 40.sp
         )
--- Jetpack Compose/Unit4/My City/app/src/main/java/com/example/mycity/ui/screen/UtilComponent.kt
+++ Jetpack Compose/Unit4/My City/app/src/main/java/com/example/mycity/ui/screen/UtilComponent.kt
@@ -3,15 +3,17 @@ package com.example.mycity.ui.screen
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Divider
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -24,26 +26,35 @@ fun Tab(
     onClick: (Int) -> Unit,
     index: Int = 0
 ) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable {
-                onClick(index)
-            }
+    Card(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 10.dp
+        )
     ) {
-        Image(
-            painter = painterResource(id = image),
-            contentDescription = stringResource(id = name),
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
-                .size(130.dp)
-                .padding(end = 20.dp)
-        )
-        Text(
-            text = stringResource(id = name),
-            fontSize = 24.sp,
-            lineHeight = 40.sp
-        )
+                .fillMaxWidth()
+                .clickable {
+                    onClick(index)
+                }
+        ) {
+            Image(
+                painter = painterResource(id = image),
+                contentDescription = stringResource(id = name),
+                modifier = Modifier
+                    .height(140.dp)
+                    .padding(end = 21.dp)
+                    .weight(1.5f),
+                contentScale = ContentScale.FillBounds,
+            )
+            Text(
+                text = stringResource(id = name),
+                fontSize = 24.sp,
+                lineHeight = 40.sp,
+                modifier = Modifier.weight(2f)
+            )
+        }
     }
-    Divider(color = Color.Black, thickness = 1.dp)
+    Spacer(modifier = Modifier.padding(15.dp))
 }
