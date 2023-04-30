694f93eefc52d1537d7025d11842608cc6b6c1c7
Poro <thinhyolo1994@gmail.com>
2023-04-30 13:57:23 +0700

    Update back button in top appbar

--- Jetpack Compose/Unit4/My City/app/src/main/java/com/example/mycity/MainActivity.kt
+++ Jetpack Compose/Unit4/My City/app/src/main/java/com/example/mycity/MainActivity.kt
@@ -3,25 +3,25 @@ package com.example.mycity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
@@ -69,11 +69,15 @@ fun MyCityApp(
     val recommendedList = listOfNotNull(DataSource.abc[index.categoryIndex])[0]
     Scaffold(
         topBar = {
-            TopAppBar(
+            AppBar(
                 currentScreen = currentScreen,
                 categoryIndex = index.categoryIndex,
                 detailIndex = index.detailIndex,
                 recommendedList = recommendedList,
+                navController = navController,
+                onClick = {
+                    navController.popBackStack()
+                }
             )
         }
     ) {
@@ -111,12 +115,15 @@ fun MyCityApp(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopAppBar(
+fun AppBar(
     currentScreen: Screen,
     categoryIndex: Int,
     detailIndex: Int,
-    recommendedList: List<Recommendation>
+    recommendedList: List<Recommendation>,
+    onClick: () -> Unit,
+    navController: NavHostController
 ) {
     lateinit var title: String
     if (currentScreen.name == Screen.CategoryScreen.name) {
@@ -126,20 +133,49 @@ fun TopAppBar(
     } else {
         title = "Category"
     }
-    Row(
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(10.dp)
-    ) {
-        Text(
-            text = title,
-            fontSize = 30.sp,
-            fontWeight = FontWeight.Bold,
-            lineHeight = 40.sp
-        )
-    }
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                fontSize = 30.sp,
+                fontWeight =
+                FontWeight.Bold,
+                lineHeight = 40.sp
+            )
+        },
+        modifier = Modifier,
+        navigationIcon = {
+            if(navController.previousBackStackEntry != null) {
+                IconButton(onClick = onClick) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = null,
+                    )
+                }
+            }
+        }
+    )
+    /*    Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        ) {
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = null,
+                )
+            }
+            Text(
+                text = title,
+                fontSize = 30.sp,
+                fontWeight =
+                FontWeight.Bold,
+                lineHeight = 40.sp
+                )
+        } */
 }
 
 @Preview(showBackground = true)
